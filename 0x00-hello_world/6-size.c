50f4cd51c30a0116178b642fbdf11da964e615af
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-16 09:19:10 -0800

    fixed bug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,7 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(n));
 	printf("Size of an int: %lu byte(s)\n", sizeof(j));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(k));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
