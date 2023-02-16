6293ea63d1706203750c5ad6a67476100617fd84
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-16 08:47:13 -0800

    Added c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Main - prints a message
+ * Return - return 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
