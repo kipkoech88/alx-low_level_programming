c76c140017d88388e830f6b6b01661f11b6eb758
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-17 09:54:51 -0800

    fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,12 +11,12 @@
 int main(void)
 {
 	int n;
-	int l;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	l = n % 10;
+	last = n % 10;
 
 	if (last > 5)
 	{
