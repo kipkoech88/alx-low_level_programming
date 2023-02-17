5c80a8208f9a2cb783c7312ef65d25de929ae22d
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-17 11:10:25 -0800

    added script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		putchar(k + '0');
+	}
+	putchar('\n');
+			
+	return (0);
+}
