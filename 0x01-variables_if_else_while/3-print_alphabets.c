27762a7d6ca4594ccba1cc9227c9b58a8675ae2e
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-17 10:45:41 -0800

    added a script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in small
+ * and caps
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	for (alph = 'A'; alph <= 'Z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
