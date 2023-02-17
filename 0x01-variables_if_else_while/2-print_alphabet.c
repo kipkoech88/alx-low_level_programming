475bd836d96f2c103771a2862a2113e24a38936c
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-17 10:08:25 -0800

    added a file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char first;
+
+	for (first = 'a'; first <= 'z'; first++)
+	{
+		putchar(first);
+	}
+
+	return (0);
+}
