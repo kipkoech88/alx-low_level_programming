dcc3f212ddc4cef023bdf3527b963cf6f2c68131
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-21 06:33:24 -0800

    fixed function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * print_alphabet: - prints lower case alphabets
- *
- * Return: 0 (Success)
- */
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
- * print_alphabet - prints lowercase alphabet
+ * print_alphabet - prints all lowercase letters
  *
  * Return: void
  */
@@ -30,5 +14,5 @@ void print_alphabet(void)
 	{
 		_putchar(a);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
