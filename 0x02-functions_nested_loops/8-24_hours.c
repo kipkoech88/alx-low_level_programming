2a55e5a7975936f82e18796888aa4ee5ccdde9e2
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-21 12:47:03 -0800

    updated main.h

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int hour,minutes;
 
-	for (hour >= 0; hour <= 23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minutes >= 0; minutes <= 59; minutes++)
+		for (minutes = 0; minutes <= 59; minutes++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
