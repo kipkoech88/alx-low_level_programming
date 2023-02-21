d03d6b8fabaf19ace3a4f390eef27ef7eb7105fa
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-21 06:01:49 -0800

    Fixed program

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - Entry point
@@ -9,9 +8,15 @@
 
 int main(void)
 {
-	char c;
-
-	_putchar(c);
+	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
