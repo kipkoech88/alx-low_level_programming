d84fbc887666880e7a26755779cb547029b0d5f4
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-22 16:48:33 -0800

    added file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all natural numbers that are
+ * multiples of 3
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a, m, s;
+
+	s = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (((a % 3) == 0) || (a % 5) == 0)
+		{
+			s += a;
+		}
+	}
+	printf("%d\n", s);
+	return (0);
+}
