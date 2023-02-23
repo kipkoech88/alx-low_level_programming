afb273107423faab50645006120cb600f882c073
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-23 07:35:54 -0800

    added files

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ * except 2 and 4
+ * Return: 0 success
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if ((a != '2') || (a != '4'))
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
