5615c68cf910ee0eaaa695b6fd2c47ceeff76dd6
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-23 22:42:49 -0800

    added files

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints square in the terminal
+ * @size: size of the square to be printed
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int a, i;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (i = 0; i < a; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
