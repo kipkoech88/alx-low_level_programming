7bafd3ac1c8010c102d2467134101a3dd8d22f6b
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-25 03:08:56 -0800

    added a file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number from 1 to 100
+ * where if number is divisible by 3 it 
+ * prints Fizz and where number is divisible by 5
+ * it prints Buzz
+ * but for multiples of 3 and 5
+ * it prints FizzBuzz
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for ( i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
