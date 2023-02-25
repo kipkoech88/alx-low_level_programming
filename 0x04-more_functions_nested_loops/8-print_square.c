e618767a5c39dbf97b0b9953c56bd6a387a5cca2
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-25 02:51:01 -0800

    fixed a bug

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_square - prints square in the terminal
  * @size: size of the square to be printed
@@ -14,12 +13,13 @@ void print_square(int size)
 	{
 		for (a = 0; a < size; a++)
 		{
-			for (i = 0; i < a; i++)
+			for (i = 0; i < size; i++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 	else
 		_putchar('\n');
