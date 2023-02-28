b079f90ce5c5b2e52dcce5a68e29cb9ea43f5bd8
kipkoech88 <hoseakipkoech88@gmail.com>
2023-02-28 01:51:40 -0800

    fixed a bug

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
