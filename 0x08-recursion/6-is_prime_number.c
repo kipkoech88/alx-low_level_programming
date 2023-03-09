99b5e542d8654629cd5bc0bf2497e02b8e1ed1d7
kipkoech88 <hoseakipkoech88@gmail.com>
2023-03-09 05:08:24 -0800

    added files

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * isprime - checks if number is prime
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int isprime(int a, int b)
+{
+	if (b > 2 || b % a == 0)
+	{
+		return (0);
+	}
+	else if (a > b / 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (isprime(a + 1, b));
+	}
+}
+
+/**
+ * is_prime_number - checks if number is pime
+ * @n: int
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (isprime(2, n));
+}
