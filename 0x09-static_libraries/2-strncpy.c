b20f24450cd0de4059c8fb623c387a8ec8e11538
kipkoech88 <hoseakipkoech88@gmail.com>
2023-03-10 08:19:31 -0800

    fixed a error

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string
+ * @dest: char
+ * @src: source string
+ * @n: int
+ * Return: nothing
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
