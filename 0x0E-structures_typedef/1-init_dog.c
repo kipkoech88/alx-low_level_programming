c7c1f827457eb2ed811d2586d8277e0e31a38e70
kipkoech88 <hoseakipkoech88@gmail.com>
2023-03-27 14:46:05 -0700

    fixed linting error

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * init_dog - initializes dog struct
- * @d - dog structure
+ * @d: dog structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -17,5 +17,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	};
+	}
 }
