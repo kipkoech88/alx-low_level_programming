47e776bc9a78ecf51c4db40ca711c866495515dc
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-07 08:50:23 -0700

    initial commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints length of a string
+ * @s: string;
+ * Return: nothing
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+
+}
