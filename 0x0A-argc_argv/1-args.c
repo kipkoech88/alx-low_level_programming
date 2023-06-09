e5a880cecdd79978a51ecd28bc3f5d7797fdd69f
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-09 02:53:24 -0700

    initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of command line argumentd
+ * @argc: argument count
+ * @argv: argument array vector
+ * Return: Always (0);
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
